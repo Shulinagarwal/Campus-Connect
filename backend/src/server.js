3bdce5e44c62bed7e449794130fc64beb92e6a96
Shulinagarwal <shulinagarwal2006@gmail.com>
2026-03-28 12:05:08 +0530

    Vercel Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ const rootDir = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://campus-connect-drab-rho.vercel.app/",
   credentials: true,
 }));
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3001/api" : "/api"
+const BASE_URL = import.meta.env.MODE === "development" ? "https://campus-connect-nepw.vercel.app/api" : "/api"
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
